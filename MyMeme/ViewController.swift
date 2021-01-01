f14f8bd07908f9736208191b4d536edfb134f17c
gondaimgano <gondaimgano@gmail.com>
2021-01-01 14:18:32 +0200

    Initial Commit

---
+++ MyMeme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMeme
+//
+//  Created by Gondai Mgano on 1/1/2021.
+//  Copyright © 2021 Gondai Mgano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
